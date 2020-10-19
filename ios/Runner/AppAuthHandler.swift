01bea7609096998a57f8ffaa766237520ccfd03d
Thorsten Claus <thorstenclaus@web.de>
2020-10-20 00:40:04 +0200

    Dialog appears

--- ios/Runner/AppAuthHandler.swift
+++ ios/Runner/AppAuthHandler.swift
@@ -10,27 +10,32 @@ import AppAuth
 import Foundation
 import Flutter.FlutterChannels
 import os.log
-import AwaitKit
-import PromiseKit
+
+typealias PostRegistrationCallback = (_ configuration: OIDServiceConfiguration?, _ registrationResponse: OIDRegistrationResponse?) -> Void
 
 
-class AppAuthHandler {
+class AppAuthHandler : NSObject {
     
     enum AuthError: Error {
         case runtimeError(String)
     }
     
-    let APP_AUTH_SESSION_CHANNEL = "insporation/appauth_authorization_events"
-    let APP_AUTH_REDIRECT_URI =  URL.init(string: "eu.jhass.insporation://callback")
-    let TIMEOUT = 6 // Seconds
+    private var authState : OIDAuthState?
+    let kAppAuthExampleAuthStateKey = "authState" // plus name?
     
+    let APP_AUTH_SESSION_CHANNEL = "insporation/appauth_authorization_events"
+    let APP_AUTH_REDIRECT_URI =  URL.init(string: "eu.jhass.insporation://callback")!
+    let TIMEOUT = 6 // Seconds
+    let controller : UIViewController
+    var registration : Registration?
     let authMethodChannel : FlutterMethodChannel
     
     // private var awaitingAuthorizationResult: Channel<AuthorizationResult<AuthorizationResponse>>? = null
-    private var awaitingAuthorizationResult: FlutterEventChannel?
     
-    init(methodChannel: FlutterMethodChannel) {
+    init(methodChannel: FlutterMethodChannel, controller: UIViewController) {
         self.authMethodChannel = methodChannel
+        self.controller = controller
+        
         print("Init App Auth Handler")
         
     }
@@ -39,9 +44,8 @@ class AppAuthHandler {
      Gets a token from auth
      */
     func getAccessTokens(_ session : Session) -> [String: String]! {
-        // mapOf("accessToken" to accessToken, "idToken" to idToken)
         var currentSession = session
-        var state = session.state
+        let state = session.state
         
         if (state == nil) {
             if #available(iOS 10.0, *) {
@@ -57,30 +61,18 @@ class AppAuthHandler {
     }
     
     func authorizeUser(userId: String, scopes: String) -> Session {
-        if awaitingAuthorizationResult != nil {
-            if #available(iOS 10.0, *) {
-                os_log("Tried to trigger multiple authorizations concurrently")
-            } else {
-                // Fallback on earlier versions
-                NSLog("Tried to trigger multiple authorizations concurrently")
-            }
-            return Session()
-        }
         
         guard let registration = RegistrationStore.fetchRegistration(userId: userId) else { return Session() }
         guard let hostname = registration.host else {return Session()}
         
         do {
-            try register(hostname: hostname)
+            try register(hostname: hostname, scopes: scopes)
         } catch {}
         
         return Session()
     }
     
-    
-    var registration : Registration
-    
-    func register(hostname: String) throws  {
+    func register(hostname: String, scopes: String) throws  {
         
         // discovers endpoints
         let discoveryURL = URL(string: "https://\(hostname)")!
@@ -98,19 +90,36 @@ class AppAuthHandler {
                 return
             }
             
+            guard let configuration = configuration else { return }
+            
             if #available(iOS 10.0, *) {
                 os_log("Discovered service config for %{public}@", log: .default, type: .debug, hostname)
             } else {
                 NSLog("Discovered service config")
             }
-    
-            self.registrationRequest(configuration!)
+            
+            self.doRegistrationRequest(configuration: configuration, callback: { (configuration, response) in
+                print("Starting with Authorization: \(String(describing: configuration))")
+                
+                // get Client from respnse
+                guard let configuration = configuration, let clientID = response?.clientID else {
+                    print("Error retrieving configuration OR clientID")
+                    return
+                }
+                
+                self.doAuthWithAutoCodeExchange(configuration: configuration,
+                                                scopes: scopes,
+                                                clientID: clientID,
+                                                clientSecret: response?.clientSecret)
+                
+            })
             
         }
     }
     
-    func registrationRequest( _ configuration : OIDServiceConfiguration) {
+    func doRegistrationRequest(configuration : OIDServiceConfiguration, callback: @escaping PostRegistrationCallback) {
         
+        print("Starting registration request")
         
         let registrationRequest = OIDRegistrationRequest(configuration: configuration,
                                                          redirectURIs: [APP_AUTH_REDIRECT_URI],
@@ -118,10 +127,111 @@ class AppAuthHandler {
                                                          grantTypes: nil,
                                                          subjectType: nil,
                                                          tokenEndpointAuthMethod: nil,
-                                                         additionalParameters: ["client_name" : "insporation*"])
-        let service = OIDAuthorizationService()
+                                                         additionalParameters: [:])
         
+        OIDAuthorizationService.perform(registrationRequest) { response, error in
+            
+            if let regResponse = response {
+                self.setAuthState(OIDAuthState(registrationResponse: regResponse))
+                print("Got registration response: \(regResponse)")
+                
+                callback(configuration, regResponse)
+                
+            } else {
+                print("Registration error: \(error?.localizedDescription ?? "DEFAULT_ERROR")")
+                self.setAuthState(nil)
+            }
+        }
+    }
+
+    func doAuthWithAutoCodeExchange(configuration: OIDServiceConfiguration, scopes: String,  clientID: String, clientSecret: String?) {
+
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            print("Error accessing AppDelegate")
+            return
+        }
+
+        let scopeArray = scopes.components(separatedBy: " ")
+        
+        // builds authentication request
+        let request = OIDAuthorizationRequest(configuration: configuration,
+                                              clientId: clientID,
+                                              clientSecret: clientSecret,
+                                              scopes: scopeArray,
+                                              redirectURL: APP_AUTH_REDIRECT_URI,
+                                              responseType: OIDResponseTypeCode,
+                                              additionalParameters: nil)
+        
+        // performs authentication request
+        print("Initiating authorization request with scope: \(request.scope ?? "DEFAULT_SCOPE")")
+
+        appDelegate.currentAuthorizationFlow = OIDAuthState.authState(byPresenting: request, presenting: self.controller) { authState, error in
+
+            if let authState = authState {
+                self.setAuthState(authState)
+                print("Got authorization tokens. Access token: \(authState.lastTokenResponse?.accessToken ?? "DEFAULT_TOKEN")")
+            } else {
+                print("Authorization error: \(error?.localizedDescription ?? "DEFAULT_ERROR")")
+                self.setAuthState(nil)
+            }
+        }
+    }
+}
+
+extension AppAuthHandler : OIDAuthStateChangeDelegate, OIDAuthStateErrorDelegate {
+    
+    func didChange(_ state: OIDAuthState) {
+        print("Did change State")
+        setAuthState(state)
     }
     
+    func authState(_ state: OIDAuthState, didEncounterAuthorizationError error: Error) {
+        print("Received authorization error: \(error)")
+    }
     
 }
+
+/// Load / Save Chnage States
+extension AppAuthHandler {
+    
+    func saveState() {
+        
+        var data: Data? = nil
+        
+        if let authState = self.authState {
+            data = NSKeyedArchiver.archivedData(withRootObject: authState)
+        }
+        
+        // TODO: Set correct defaults for State !
+        
+        if let userDefaults = UserDefaults(suiteName: "group.net.openid.appauth.Example") {
+            userDefaults.set(data, forKey: kAppAuthExampleAuthStateKey)
+            userDefaults.synchronize()
+        }
+    }
+    
+    func loadState() {
+        guard let data = UserDefaults(suiteName: "group.net.openid.appauth.Example")?.object(forKey: kAppAuthExampleAuthStateKey) as? Data else {
+            return
+        }
+        
+        if let authState = NSKeyedUnarchiver.unarchiveObject(with: data) as? OIDAuthState {
+            self.setAuthState(authState)
+        }
+    }
+    
+    func setAuthState(_ authState: OIDAuthState?) {
+        if (self.authState == authState) {
+            return;
+        }
+        self.authState = authState;
+        self.authState?.stateChangeDelegate = self;
+        self.stateChanged()
+    }
+    
+    func stateChanged() {
+        self.saveState()
+        // self.updateUI()
+    }
+}
+
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -12,39 +12,55 @@ import os.log
     private let APP_AUTH_CHANNEL = "insporation/appauth"
     var appAuthHandler : AppAuthHandler?
     
+    
     override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let authChannel = FlutterMethodChannel(name: APP_AUTH_CHANNEL,
-                                           binaryMessenger: controller.binaryMessenger)
-    
-    appAuthHandler = AppAuthHandler(methodChannel: authChannel)
-    authChannel.setMethodCallHandler {(call: FlutterMethodCall, result: @escaping FlutterResult) in
-    
-        guard call.method == "getAccessToken" else {
-            result(FlutterMethodNotImplemented)
-            return
-        }
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        if let arguments = call.arguments {
-            guard let sessionArgs = (arguments as! [String:Any?])["session"] else {
-                result("Invalid Arguments (need at least a session")
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let authChannel = FlutterMethodChannel(name: APP_AUTH_CHANNEL,
+                                               binaryMessenger: controller.binaryMessenger)
+        
+        appAuthHandler = AppAuthHandler(methodChannel: authChannel, controller: controller)
+        authChannel.setMethodCallHandler {(call: FlutterMethodCall, result: @escaping FlutterResult) in
+            
+            guard call.method == "getAccessToken" else {
+                result(FlutterMethodNotImplemented)
                 return
             }
             
-            let sessionArgs_:[String: Any?] = (sessionArgs as! [String : Any?])
-            let session = Session(sessionData: sessionArgs_)
-            if let appAuthHandler = self.appAuthHandler {
-                result(appAuthHandler.getAccessTokens(session))
+            if let arguments = call.arguments {
+                guard let sessionArgs = (arguments as! [String:Any?])["session"] else {
+                    result("Invalid Arguments (need at least a session")
+                    return
+                }
+                
+                let sessionArgs_:[String: Any?] = (sessionArgs as! [String : Any?])
+                let session = Session(sessionData: sessionArgs_)
+                if let appAuthHandler = self.appAuthHandler {
+                    result(appAuthHandler.getAccessTokens(session))
+                }
             }
+            
+            result("Failure running args code")
         }
         
-        result("Failure running args code")
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    // Handle redirect from website
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        print("Handle redirect")
+        
+        if let authorizationFlow = self.currentAuthorizationFlow, authorizationFlow.resumeExternalUserAgentFlow(with: url) {
+            self.currentAuthorizationFlow = nil
+            return true
+        }
+        
+        return false
+    }
+    
 }
